337765c7746fe7dee62eb87f2839f9834d485893
philipborg <philip.borgstrom@gmail.com>
2018-09-30 15:26:09 +0200

    build.gradle creation

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    `build-scan`
+    kotlin("jvm") version "1.2.71"
+}
+
+val junitVersion = "5.3.1";
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib", "1.2.31"))
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+buildScan {
+    setLicenseAgreementUrl("https://gradle.com/terms-of-service") 
+    setLicenseAgree("yes")
+
+    publishAlways() 
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
