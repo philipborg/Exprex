5e6a6bdd064e54616b362a84fcf6d3a5f2507696
philipborg <philip.borgstrom@gmail.com>
2018-09-30 16:15:44 +0200

    Remove FastUtil
    Accidentally added to the wrong project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
     testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-    compile(group = "it.unimi.dsi", name = "fastutil", version = "8.2.2")
 }
 
 buildScan {
