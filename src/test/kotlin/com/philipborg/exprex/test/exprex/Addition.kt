af37944aeb241d96a0df114cdbb9b58ab1ad5361
philipborg <philip.borgstrom@gmail.com>
2018-10-06 16:23:33 +0200

    AdditionExprex test

---
+++ src/test/kotlin/com/philipborg/exprex/test/exprex/Addition.kt
@@ -0,0 +1,31 @@
+package com.philipborg.exprex.test.exprex
+
+import com.philipborg.exprex.operators.AdditionExprex
+import com.philipborg.exprex.toValueExprex
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.function.Executable
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Addition {
+    @Test
+    fun simpleAdd() {
+        Assertions.assertAll(
+                arrayOf(
+                        Scenario(1, 1, 2),
+                        Scenario(1, 2, 3),
+                        Scenario(0, 0, 0),
+                        Scenario(1, -1, 0),
+                        Scenario(-1, -2, -3),
+                        Scenario(2, -3, -1)
+                ).map {
+                    Executable {
+                        Assertions.assertEquals(it.sum.toBigInteger(), AdditionExprex(it.augend.toValueExprex(), it.addend.toValueExprex()).invoke())
+                    }
+                }
+        )
+    }
+
+    data class Scenario(val augend: Int, val addend: Int, val sum: Int)
+}
