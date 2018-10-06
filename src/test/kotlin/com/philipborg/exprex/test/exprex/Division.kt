5b80505f3806dff04e7c926a04c0dbf6676c55fa
philipborg <philip.borgstrom@gmail.com>
2018-10-06 22:06:26 +0200

    Few DivisionExprex tests
    These are just a few to test basic functionality. A more comphrehensive test can be found in BigIntegerDivisions

---
+++ src/test/kotlin/com/philipborg/exprex/test/exprex/Division.kt
@@ -0,0 +1,32 @@
+package com.philipborg.exprex.test.exprex
+
+import com.philipborg.exprex.operators.DivisionExprex
+import com.philipborg.exprex.toValueExprex
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.function.Executable
+import java.math.RoundingMode
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Division {
+    @Test
+    fun simpleDivision() {
+        Assertions.assertAll(
+                arrayOf(
+                        Scenario(10, 2, RoundingMode.UNNECESSARY, 5),
+                        Scenario(5, 2, RoundingMode.HALF_DOWN, 2),
+                        Scenario(-1, 2, RoundingMode.DOWN, 0),
+                        Scenario(-5, -2, RoundingMode.HALF_EVEN, 2)
+                ).map {
+                    Executable {
+                        Assertions.assertEquals(it.quotient.toBigInteger(), DivisionExprex(it.dividend.toValueExprex(), it.divisor.toValueExprex(), it.roundingMode).invoke(), it.toString())
+                    }
+                }
+        )
+        Assertions.assertThrows(ArithmeticException::class.java, { DivisionExprex(1.toValueExprex(), 0.toValueExprex(), RoundingMode.UNNECESSARY).invoke() }, "One divided by zero")
+        Assertions.assertThrows(ArithmeticException::class.java, { DivisionExprex(3.toValueExprex(), 2.toValueExprex(), RoundingMode.UNNECESSARY).invoke() }, "Three divided by two RoundingMode='${RoundingMode.UNNECESSARY.name}'")
+    }
+
+    data class Scenario(val dividend: Int, val divisor: Int, val roundingMode: RoundingMode, val quotient: Int)
+}
