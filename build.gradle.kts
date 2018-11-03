77b33b4c1d40264bc39e4455aa5e4e6f8aafbf40
philipborg <philip.borgstrom@gmail.com>
2018-11-03 17:12:29 +0100

    Update to 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `build-scan`
-    kotlin("jvm") version "1.2.71"
+    kotlin("jvm") version "1.3.0"
 }
 
 val junitVersion = "5.3.1";
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib", "1.2.71"))
+    implementation(kotlin("stdlib", "1.3.0"))
     testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
